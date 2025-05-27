e4fbaca5dd44e367b8521f3ddacfee83d29b1f17
Nagoluaadhi <128470729+Nagoluaadhi@users.noreply.github.com>
2025-05-27 17:22:48 +0530

    LoginPage.jsx

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -10,7 +10,7 @@ function LoginPage({ setUser }) {
 
   const handleLogin = async () => {
     try {
-      const res = await axios.post('/login', { username, password, role });
+      const res = await axios.post('api/login', { username, password, role });
       localStorage.setItem('user', JSON.stringify(res.data));
       setUser(res.data);
 
